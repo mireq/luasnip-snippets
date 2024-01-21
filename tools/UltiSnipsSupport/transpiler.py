b1cc82dfd1244d4c8c4abbadb569a9e29daae229
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-01-21 07:16:20 +0100

    feat: Added filetype mapping generation

--- tools/UltiSnipsSupport/transpiler.py
+++ tools/UltiSnipsSupport/transpiler.py
@@ -36,6 +36,27 @@ def extract_global_code_definitions(source: SnippetSource) -> dict[str, OrderedS
 	return dict(global_definitions)
 
 
+def save_filetype_mapping(source: SnippetSource, output_dir: Path):
+	filetype_mapping = {}
+	try:
+		with output_dir.joinpath('filetype_includes.txt').open('r') as fp:
+			for line in fp:
+				line = line.strip()
+				if not line:
+					continue
+				line = line.split()
+				filetype_mapping[line[0]] = set(line[1:])
+	except FileNotFoundError:
+		pass
+
+	filetype_mapping.setdefault(source.filetype, [])
+	filetype_mapping[source.filetype] = list(set(filetype_mapping[source.filetype]).union(source.extends))
+	with output_dir.joinpath('filetype_includes.txt').open('w') as fp:
+		for filetype, included_filetypes in filetype_mapping.items():
+			if not included_filetypes:
+				continue
+			fp.write(f'{filetype} {" ".join(included_filetypes)}\n')
+
 
 class ProgramArgs(typing.Protocol):
 	ultisnips_dirs: list[Path]
@@ -50,7 +71,6 @@ def run(args: ProgramArgs, filetype: str):
 		snipmate_dirs=args.snipmate_dirs
 	)
 
-	glboal_code_definitions = extract_global_code_definitions(source)
-	print(glboal_code_definitions)
+	save_filetype_mapping(source, args.output_dir)
 	#for snippet in source.get_all_snippets():
 	#	print(snippet)
